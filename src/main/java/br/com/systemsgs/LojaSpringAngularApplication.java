ea2f3ad816a5b7435855eaad97ce1e26fde67409
GuilhermeJWT <guiromanno@gmail.com>
2021-11-10 15:16:13 -0300

    Setup Inicial do Projeto

---
+++ src/main/java/br/com/systemsgs/LojaSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaSpringAngularApplication.class, args);
+	}
+
+}
