ae26b041e53bffcb2d05009284a115b43aeb3209
GuilhermeJWT <guiromanno@gmail.com>
2021-11-18 22:46:44 -0300

    Cria a entidade de Pessoa Fisica e Juridica

---
+++ src/main/java/br/com/systemsgs/model/ModelPessoaFisica.java
@@ -0,0 +1,46 @@
+package br.com.systemsgs.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class ModelPessoaFisica extends ModelPessoa{
+
+	private static final long serialVersionUID = 1L;
+	
+	@NotBlank(message = "CPF deve ser Informado!")
+	@CPF(message = "CPF Inválido!")
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+}
---
+++ src/main/java/br/com/systemsgs/model/ModelPessoaJuridica.java
@@ -0,0 +1,77 @@
+package br.com.systemsgs.model;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class ModelPessoaJuridica extends ModelPessoa{
+
+	private static final long serialVersionUID = 1L;
+	
+	@CNPJ(message = "CNPJ Inválido!")
+	private String cnpj;
+	
+	private String inscEstadual;
+	
+	private String inscMunicipal;
+	
+	private String nomeFantasia;
+	
+	private String razaoSocial;
+	
+	private String categoria;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscEstadual() {
+		return inscEstadual;
+	}
+
+	public void setInscEstadual(String inscEstadual) {
+		this.inscEstadual = inscEstadual;
+	}
+
+	public String getInscMunicipal() {
+		return inscMunicipal;
+	}
+
+	public void setInscMunicipal(String inscMunicipal) {
+		this.inscMunicipal = inscMunicipal;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+}
